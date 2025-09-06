8e1141c198d54ac5e5b8a30da6b65ddd5f264015
Irsyad1609 <irsyadvekok@gmail.com>
2025-09-06 11:17:59 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/logo.svg" type="image/svg+xml" />
+      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
         <Analytics />
